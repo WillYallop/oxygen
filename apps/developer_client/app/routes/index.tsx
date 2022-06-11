dce32f1118dd763b337455582b5323e671594903
WillYallop <hello@williamyallop.com>
2022-06-11 13:10:44 +0100

    [style]: added basic scss setup to ui-styles package

--- apps/developer_client/app/routes/index.tsx
+++ apps/developer_client/app/routes/index.tsx
@@ -1,17 +1,17 @@
 import { Button } from 'ui';
 
 const HomePage = () => {
-  return (
-    <div
-      style={{
-        fontFamily: 'system-ui, sans-serif',
-        lineHeight: '1.4',
-      }}
-    >
-      <h1>Welcome to Remix</h1>
-      <Button></Button>
-    </div>
-  );
+    return (
+        <div
+            style={{
+                fontFamily: 'system-ui, sans-serif',
+                lineHeight: '1.4',
+            }}
+        >
+            <h1>Welcome to Remix</h1>
+            <Button></Button>
+        </div>
+    );
 };
 
 export default HomePage;
